da90cf207a019535b358f867eeb9144122170aee
Saravanan s <79571256+saravanans1204@users.noreply.github.com>
2024-03-09 12:57:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,31 +9,31 @@ app.get('/', async (req, res) => {
     res.send('<h1>happening</h1>')
 });
 
-// // routers
-// app.get('/api', async (req, res) => {
-//     // Data to be inserted
-//     const data1 = {
-//         _id: '65eb11a2be8309f353afdb81',
-//         message: 'Hello from MongoDB!'
-//     };
+// routers
+app.get('/api', async (req, res) => {
+    // Data to be inserted
+    const data1 = {
+        _id: '65eb11a2be8309f353afdb81',
+        message: 'Hello from MongoDB!'
+    };
 
-//     res.send('<h1>happening</h1>')
-// });
+    res.send('<h1>happening</h1>')
+});
 
-// app.get('/api/getData', async (req, res) => {
+app.get('/api/getData', async (req, res) => {
  
-//     loginDb.find({ "message": { $exists: true } }).then((resolve,reject)=>{
-//         // console.log(resolve)
-//         if(resolve!==null){    
-//             res.json(resolve[0].message);
-//         }else{
-//             const jsonData = { message: 'Error 500' };
-//             res.json(jsonData);
+    loginDb.find({ "message": { $exists: true } }).then((resolve,reject)=>{
+        // console.log(resolve)
+        if(resolve!==null){    
+            res.json(resolve[0].message);
+        }else{
+            const jsonData = { message: 'Error 500' };
+            res.json(jsonData);
 
-//         }
-//     })
+        }
+    })
     
-// });
+});
 
 // app.get('*', function(req, res){
 //     if(res.status(404)){
